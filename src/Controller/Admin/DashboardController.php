d09974700bf3cdf86b53159741d1ca2a7507efeb
Ramzii-Dev <ramzi446@hotmail.com>
2021-04-20 14:44:56 +0200

    17/04/2021

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Anime;
+use App\Entity\Carrier;
 use App\Entity\Episode;
 use App\Entity\Genre;
 use App\Entity\Product;
@@ -12,6 +13,7 @@ use App\Entity\VideoCategory;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -35,11 +37,22 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('utilisateurs', 'fas fa-user', User::class);
-        yield MenuItem::linkToCrud('category-produit', 'fas fa-th-large', ProductCategory::class);
-        yield MenuItem::linkToCrud('produit', 'fas fa-tags', Product::class);
-        yield MenuItem::linkToCrud('category-anime', 'fas fa-photo-video', VideoCategory::class);
-        yield MenuItem::linkToCrud('genre', 'far fa-object-group', Genre::class);
-        yield MenuItem::linkToCrud('episode', '	fas fa-tasks', Episode::class);
-        yield MenuItem::linkToCrud('anime', '	fas fa-video', Anime::class);
+
+        yield MenuItem::subMenu('Boutique', 'fa fa-article')->setSubItems([
+         MenuItem::linkToCrud('category-produit', 'fas fa-th-large', ProductCategory::class),
+         MenuItem::linkToCrud('livraison', 'fas fa-truck', Carrier::class),
+         MenuItem::linkToCrud('produit', 'fas fa-tags', Product::class)
+         ]);
+        yield MenuItem::subMenu('Anime', 'fa fa-article')->setSubItems([
+         MenuItem::linkToCrud('category-anime', 'fas fa-photo-video', VideoCategory::class),
+         MenuItem::linkToCrud('genre', 'far fa-object-group', Genre::class),
+         MenuItem::linkToCrud('episode', '	fas fa-tasks', Episode::class),
+         MenuItem::linkToCrud('anime', '	fas fa-video', Anime::class),
+        ]);
     }
+
+    public function configureAssets(): Assets
+    {
+        return Assets::new()->addCssFile('css/admin.css');
+     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,6 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+
+
+
+
     #[Route('/', name: 'home')]
     public function index(): Response
     {
